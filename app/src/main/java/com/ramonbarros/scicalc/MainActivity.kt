eb857b51ebeb7f6cc03f5734a12ba168a3755132
Ramon Barros <pbarrosramon@gmail.com>
2020-11-15 17:18:30 -0300

    solve bugs with calculation and add MATHERROR as result for bad math

--- app/src/main/java/com/ramonbarros/scicalc/MainActivity.kt
+++ app/src/main/java/com/ramonbarros/scicalc/MainActivity.kt
@@ -2,13 +2,13 @@ package com.ramonbarros.scicalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.core.text.isDigitsOnly
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.NumberFormatException
-import java.math.BigInteger
 import kotlin.Exception
 import kotlin.math.pow
 
@@ -17,154 +17,74 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /*// number 0 to 9 pressed
-        button_0.setOnClickListener() {
-            val digit = button_0.text.toString()
-            val formulaDigit = button_0.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_1.setOnClickListener() {
-            val digit = button_1.text.toString()
-            val formulaDigit = button_1.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_2.setOnClickListener() {
-            val digit = button_2.text.toString()
-            val formulaDigit = button_2.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_3.setOnClickListener() {
-            val digit = button_3.text.toString()
-            val formulaDigit = button_3.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_4.setOnClickListener() {
-            val digit = button_4.text.toString()
-            val formulaDigit = button_4.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_5.setOnClickListener() {
-            val digit = button_5.text.toString()
-            val formulaDigit = button_5.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_6.setOnClickListener() {
-            val digit = button_6.text.toString()
-            val formulaDigit = button_6.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_7.setOnClickListener() {
-            val digit = button_7.text.toString()
-            val formulaDigit = button_7.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_8.setOnClickListener() {
-            val digit = button_8.text.toString()
-            val formulaDigit = button_8.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_9.setOnClickListener() {
-            val digit = button_9.text.toString()
-            val formulaDigit = button_9.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        // Basic Operation Buttons
-
-        button_sum.setOnClickListener(){
-            val digit = button_sum.text.toString()
-            val formulaDigit = button_sum.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_subtraction.setOnClickListener(){
-            val digit = button_sum.text.toString()
-            val formulaDigit = button_sum.text.toString()
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_division.setOnClickListener(){
-            val digit = button_sum.text.toString()
-            val formulaDigit = '/'
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }
-
-        button_multiplication.setOnClickListener(){
-            val digit = button_sum.text.toString()
-            val formulaDigit = '*'
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, digit, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaDigit, isMaxCharacter) // update display string and checks max character
-            buttonClicked(isMaxCharacter, digit, formulaDigit, displayText) // do the next steps, refer to it
-        }*/
-
+        tvDisplayCalc.movementMethod = ScrollingMovementMethod();
     }
 
     var isResult = false;
-    val maxCharacters: Int = 28
+    val maxCharacters: Int = 90
     var displayText: String = "";
     var displayFormula: String = "";
     var isMaxCharacter: Boolean = false;
     var formulaInput: String = ""
     var displayOutput: String = ""
+    var lastInput: String = ""
+    var lastResult = ""
     var mathError: Boolean = false;
+
 
     fun resetDisplay() {
         if (isResult) {
-            tvDisplayCalc.text = tvDisplayResult.text
-            displayFormula = tvDisplayResult.text.toString()
-            displayOutput = tvDisplayResult.text.toString()
-            displayText = tvDisplayResult.text.toString()
+            if (!mathError) {
+                var i = lastResult.split('.')[0]
+                var f = lastResult.split('.')[1]
+                if (Math.floor(f.toDouble()) == 0.0) {
+                    lastResult = i.toInt().toString()
+                } else {
+                    lastResult = tvDisplayResult.text.toString().toBigDecimal().toPlainString()
+                }
+            } else {
+                lastResult = ""
+            }
+            displayFormula = lastResult
+            displayText = lastResult
+            tvDisplayCalc.text = lastResult
             tvDisplayResult.text = "0"
             isResult = false
+            mathError = false
+        }
+    }
+
+    fun eraseDisplay() {
+        isResult = false
+        tvDisplayResult.text = "0"
+        tvDisplayCalc.text = ""
+        displayText = ""
+        displayFormula = ""
+    }
+
+    fun eraseLastInput() {
+        if (tvDisplayCalc.text.count() > 0) {
+            val lastDisplayCharacter = changeToOutputOrFormula(lastInput, false)
+            val lastFormulaCharacter = changeToOutputOrFormula(lastInput, true)
+            val lastCharacter = tvDisplayCalc.text[tvDisplayCalc.text.lastIndex]
+            Log.e("Teste", "Last character is $lastCharacter")
+            tvDisplayCalc.text = tvDisplayCalc.text.dropLast(lastDisplayCharacter.length)
+            displayText = displayText.dropLast(lastDisplayCharacter.length)
+            displayFormula = displayFormula.dropLast(lastFormulaCharacter.length)
+            Log.e("Teste", "Formula = $displayFormula e Display = " + tvDisplayCalc.text)
+        }
+    }
+
+    fun calculateButtonPressed() {
+        var a = displayText
+        var b = a.count()
+        if (displayText.count() > 0) {
+            isResult = true
+            var result = showResult(displayFormula)
+            if (result == "MATH.ERROR"){
+                mathError = true
+            }
+            tvDisplayResult.text = result
         }
     }
 
@@ -173,22 +93,29 @@ class MainActivity : AppCompatActivity() {
         val button = view as Button
         val input = button.text.toString()
         if (input == "=") {
-            isResult = true
-            tvDisplayResult.text = showResult(displayFormula)
+            calculateButtonPressed()
+        } else if (input == "AC") {
+            eraseDisplay()
+        } else if (input == "\u232B") {
+            Log.e("Teste", "Pressed $input")
+            eraseLastInput()
         } else {
+            lastInput = input
             displayOutput =  changeToOutputOrFormula(input, false) // change the keyboard input to be shown in calculator display
             formulaInput = changeToOutputOrFormula(input, true) // change the keyboard input to a formula input
-            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // update if maxCharacter is fulfilled
-            displayText = buttonToString(displayText, displayOutput, isMaxCharacter) // update display string and checks max character
-            displayFormula = buttonToFormula(displayFormula, formulaInput, isMaxCharacter) // update display string and checks max character
+            isMaxCharacter = checkIsMaxCharacters(displayText, maxCharacters) // check if maxCharacter is fulfilled
+            if (!isMaxCharacter) {
+                displayText = buttonToString(displayText, displayOutput) // update display string and checks max character
+                displayFormula = buttonToFormula(displayFormula, formulaInput) // update display string and checks max character
+            }
             refreshDisplay(displayText, isMaxCharacter)
             Log.i("teste", "O botão com texto $input foi clicado")
         }
     }
 
-    fun showResult(formulaInput: String): String {
-        Log.e( "Actual","formula is $formulaInput")
-        return calculateResult(formulaInput)
+    fun showResult(displayFormula: String): String {
+        Log.e( "Actual","formula is $displayFormula")
+        return calculateResult(displayFormula)
     }
 
     fun calculateResult(displayFormula: String):String{
@@ -196,16 +123,35 @@ class MainActivity : AppCompatActivity() {
         var isOperation = true
         substitute = insertMultiplySign(substitute)
         substitute = changePiEuler(substitute)
-        while (isOperation) {
-            substitute = resolveComplexOperations(substitute)
-            substitute = solveParenthesis(substitute)
-            substitute = solveOperations(substitute)
-            if (substitute.matches("-?\\d*\\.*\\d+?".toRegex())) {
+        var trying = 99
+        for (n in 0..trying) {
+            if (n == 99) {
                 isOperation = false
+                lastResult = "MATH.ERROR"
+            }
+            if (isOperation) {
+                try {
+                    substitute = resolveComplexOperations(substitute)
+                    substitute = solveParenthesis(substitute)
+                    substitute = solveOperations(substitute)
+
+                    if (substitute.matches("-?\\d*\\.*\\d+?".toRegex())) {
+                        isOperation = false
+                    }
+                } catch (e: Exception) {
+                    isOperation = false
+                }
+            } else {
+                try {
+                    println(substitute.toDouble())
+                    lastResult = substitute.toDouble().toString()
+                    return lastResult
+                } catch (e: Exception) {
+                    return "MATH.ERROR"
+                }
             }
         }
-        println(substitute.toDouble())
-        return substitute.toDouble().toString()
+        return lastResult
     }
 
     fun solveOperations(args: String): String {
@@ -349,7 +295,7 @@ class MainActivity : AppCompatActivity() {
             return " "
             } else {
             if (input.isDigitsOnly() || input in arrayOf("+", "-", "%", "(", ")", ".", "÷", "×", "e", "π") ){ return input }
-            else if (input in arrayOf("sin", "cos", "tan", "ln", "log", "sec", "cosec", "cotan")){ return "$input(" }
+            else if (input in arrayOf("sin", "cos", "tan", "ln", "log", "sec", "csc", "ctn")){ return "$input(" }
             else if (input == "x!"){ return "fact(" }
             else if (input == "√"){ return "sqrt(" }
             else if (input == "x^y"){ return "^" }
@@ -360,18 +306,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun buttonToString(actual:String, new:String, isMaxCharacters: Boolean): String {
-        val newString: String = actual + new;
-        if (isMaxCharacters) {
-            return actual
-        }
-        //Log.e( "Actual","actual is $actual, newString is $newString")
-        return newString
+    fun buttonToString(actual:String, new:String): String {
+        Log.e("Teste", actual+new+ " DISPLAY")
+        return actual + new;
     };
 
-    fun buttonToFormula(actual:String, new:String, maxCharacters: Boolean): String {
+    fun buttonToFormula(actual:String, new:String): String {
         val newString: String = actual + new;
-        Log.e( "Actual","actual formula is $actual, newString is $newString")
+        Log.e("Teste", actual+new+ " FORMULA")
         return newString
     }
 
